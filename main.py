1d794b3a00c4b7fe3a0a30762a3fc491412d22f9
8property <autorealproperty@gmail.com>
2025-07-24 05:50:01 +0000

    without headless

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def run_scraper():
         # === Setup headless Chrome ===
         options = Options()
         options.page_load_strategy = "eager"
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")  # Important!
